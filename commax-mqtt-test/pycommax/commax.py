3977cad3a7b2b94dab4a55ff9878e980446cd699
bykkkk <158445639+bykkkk@users.noreply.github.com>
2026-01-27 14:10:09 +0900

    Update commax.py

--- commax-mqtt-test/pycommax/commax.py
+++ commax-mqtt-test/pycommax/commax.py
@@ -114,32 +114,38 @@ def do_work(config, device_list):
         return checksum(input_hex)
 
     def make_hex_temp(k, curTemp, setTemp, state):
+        # setTemp로 들어오는 명령을 CHANGE로 처리
+        if state == 'setTemp':
+            state = 'CHANGE'
+
         if state in ['OFF', 'ON', 'CHANGE']:
             tmp_hex = device_list['Thermo'].get('command' + state)
             change = device_list['Thermo'].get('commandNUM')
+            
+            if not tmp_hex:
+                return None
+
             tmp_hex = make_hex(k, tmp_hex, change)
-            if state == 'CHANGE':
-                setT = pad(setTemp)
-                chaTnum = device_list['Thermo'].get('chaTemp')
-                tmp_hex = tmp_hex[:chaTnum - 1] + setT + tmp_hex[chaTnum + 1:]
+            
+            # ON 또는 CHANGE일 때 온도값 삽입
+            if state in ['CHANGE', 'ON']:
+                try:
+                    # 온도값이 없으면 기본값 22
+                    val = 22
+                    if setTemp and float(setTemp) != 0:
+                        val = int(float(setTemp))
+                    
+                    setT = format(val, '02X')
+                    chaTnum = device_list['Thermo'].get('chaTemp')
+                    
+                    if chaTnum:
+                        tmp_hex = tmp_hex[:chaTnum - 1] + setT + tmp_hex[chaTnum + 1:]
+                except:
+                    pass
+
             return checksum(tmp_hex)
         else:
-            tmp_hex = device_list['Thermo'].get(state)
-            change = device_list['Thermo'].get('stateNUM')
-            tmp_hex = make_hex(k, tmp_hex, change)
-            setT = pad(setTemp)
-            curT = pad(curTemp)
-            curTnum = device_list['Thermo'].get('curTemp')
-            setTnum = device_list['Thermo'].get('setTemp')
-            tmp_hex = tmp_hex[:setTnum - 1] + setT + tmp_hex[setTnum + 1:]
-            tmp_hex = tmp_hex[:curTnum - 1] + curT + tmp_hex[curTnum + 1:]
-            if state == 'stateOFF':
-                return checksum(tmp_hex)
-            elif state == 'stateON':
-                tmp_hex2 = tmp_hex[:3] + str(3) + tmp_hex[4:]
-                return [checksum(tmp_hex), checksum(tmp_hex2)]
-            else:
-                return None
+            return None
 
     def make_device_info(dev_name, device_list):
         num = device_list[dev_name].get('Number', 0)
